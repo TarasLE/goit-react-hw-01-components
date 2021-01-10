adab013e906394b2df8f7696a54fde05d7842dcd
Taras Liuzniak <lyuznyaktaras@gmail.com>
2021-01-10 15:07:47 +0200

    intsave

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -4,8 +4,8 @@ import styles from './FriendsListItem.module.css';
 export default function FriendsListItem({ avatar, name, isOnline }) {
    return (
         
-        <li className={isOnline? styles.ItemOnline : styles.ItemOfline } >
-           <span className={styles.Status}></span>
+        <li className={styles.Item} >
+           <span className={isOnline? styles.StatusOnline : styles.StatusOfline }></span>
   <img className={styles.Avatar} src={avatar} alt="" width="48" />
   <p className={styles.Name}>{name}</p>
 </li>
