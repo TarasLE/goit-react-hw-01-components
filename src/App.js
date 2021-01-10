2fc6164fe7893f9ac3f66b0389542ee68552a68c
Taras Liuzniak <lyuznyaktaras@gmail.com>
2021-01-10 16:17:17 +0200

    ht1_4 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
-import FriendList from './components/FriendList/FriendsList'
+import FriendList from './components/FriendList/FriendsList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import user from './user.json';
 import statisticalData from './statistical-data.json';
 import friends from './friends.json';
+import transactions from './transactions.json';
 
 
 export default function App() {
@@ -21,5 +23,7 @@ export default function App() {
         <Statistics stats={statisticalData} />
 
         <FriendList friends={friends} />
+
+        <TransactionHistory items={transactions} />
   </div>)
 }
---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import styles from './TransactionHistory.module.css';
+
+export default function TransactionHistory({ items }) {
+    return (
+        <table className={styles.TransactionHistory}>
+  <thead>
+    <tr>
+      <th className={styles.Header}>Type</th>
+      <th className={styles.Header} >Amount</th>
+      <th className={styles.Header}>Currency</th>
+    </tr>
+  </thead>
+        
+  <tbody>
+                
+ {items.map(item=> (
+    <tr>
+      <td className={styles.Data}>{item.type}</td>
+      <td className={styles.Data}>{item.amount}</td>
+      <td className={styles.Data}>{item.currency}</td>
+    </tr>
+            ))}
+    
+  </tbody>
+</table>
+    )
+}
+
+TransactionHistory.propTypes = {
+    items: PropTypes.array.isRequired,
+}
